ea21be5086d1c15751c88d7ed38cb3294fc53be1
IconicLuck <uhasib959@gmail.com>
2023-07-14 15:11:58 +0600

    added two buttons on the main page

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,12 @@
-
+import './Main.css'
 
 const Main = () => {
     return (
-        <div>
-            <h1 className="text-4xl text-center">This is Main layout</h1>
+        <div className="background-image-banner h-screen bg-slate-800 bg-blend-overlay text-white">
+            <div className='flex justify-center gap-10 items-center h-screen'>
+                <button className='text-2xl border-2 p-5 hover:bg-slate-200 ease-in-out duration-200 hover:text-black'>Are you a smoker?</button>
+                <button className='text-2xl border-2 p-5 hover:bg-slate-200 ease-in-out duration-200 hover:text-black'>Are you a quitter?</button>
+            </div>
         </div>
     );
 };
